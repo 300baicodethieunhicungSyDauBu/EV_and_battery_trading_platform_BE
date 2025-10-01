1e7db49b475067757268a6c32648dda02509aa9d
Trí Sỹ Trương <thefoxno4@gmail.com>
2025-10-02 02:35:37 +0700

    add jwt

--- BE.API/Controllers/UserController.cs
+++ BE.API/Controllers/UserController.cs
@@ -1,6 +1,12 @@
-﻿using BE.BOs.Models;
+﻿using BE.API.DTOs.Request;
+using BE.API.DTOs.Response;
+using BE.BOs.Models;
 using BE.REPOs.Interface;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BE.API.Controllers
 {
@@ -14,20 +20,55 @@ namespace BE.API.Controllers
         }
 
         [HttpPost("api/login")]
-        public ActionResult<User> Login([FromBody] LoginRequest request)
+        public ActionResult<LoginResponse> Login([FromBody] LoginRequest request)
         {
             var user = _userRepo.GetAccountByEmailAndPassword(request.Email, request.Password);
             if (user == null)
             {
                 return Unauthorized("Invalid email or password.");
             }
+
+
+            // Generate JWT Token
+            var token = GenerateJwtToken(user);
+            return Ok(new LoginResponse
+            {
+                Role = user.RoleId.ToString(),
+                Token = token,
+                AccountId = user.UserId.ToString()
+            });
             return Ok(user);
         }
-    }
 
-    public class LoginRequest
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        private string GenerateJwtToken(User user)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true).Build();
+
+            var claims = new List<Claim> {
+            new Claim(ClaimTypes.Name,user.FullName),
+            new Claim(ClaimTypes.Email,user.Email),
+            new Claim("UserId",user.UserId.ToString())
+            };
+
+            if (user.RoleId.HasValue)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, user.RoleId.Value.ToString()));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var preparedToken = new JwtSecurityToken(
+                            issuer: configuration["JWT:Issuer"],
+                            audience: configuration["JWT:Audience"],
+                            claims: claims,
+                            expires: DateTime.Now.AddMinutes(30),
+                            signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(preparedToken);
+
+        }
+
     }
 }
---
+++ BE.API/DTOs/Response/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BE.API.DTOs.Response
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public string AccountId { get; set; }
+    }
+}
