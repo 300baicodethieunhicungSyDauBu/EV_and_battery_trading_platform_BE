751ac10598e7a6818ea67333f6df0f28d67455e5
Trí Sỹ <thefono4@gmail.com>
2025-11-18 00:53:44 +0700

    update cors and conn string

--- BE.API/Program.cs
+++ BE.API/Program.cs
@@ -207,7 +207,7 @@ builder.Services.AddCors(options =>
             policy
                 .WithOrigins(
                     "http://localhost:5173",
-                    "https://evtrading-frontend.vercel.app",
+                    "https://swp-391-topic2-frontend-ver2.vercel.app",
                     "http://localhost:5174"
                 )
                 .AllowAnyHeader()
